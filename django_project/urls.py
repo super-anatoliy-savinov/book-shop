1f77869c60ebcc746ca2d2f7a78a87ec21525c23
Anatoliy <anatolijsavinov004@gmail.com>
2026-05-23 12:54:09 +0500

    ch6

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,5 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
