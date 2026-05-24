1a5e26c02046ad791caa35bc8f83a05033726bcf
Anatoliy <anatolijsavinov004@gmail.com>
2026-05-24 11:20:00 +0500

    update allowed hosts with exact render url

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False )
 
-ALLOWED_HOSTS = ["", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["book-shop-z38p.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
